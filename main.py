a943214abc7c5f8d97ea1dba2c03bcf9cd620a87
fly0227 <122483252+fly0227@users.noreply.github.com>
2025-03-08 23:51:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def tip():
 
 
 # 推送信息
-def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, pop, tips,pipi,date):
+def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, pop, tips,pipi):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
